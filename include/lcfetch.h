6705d1469f3c7a7f1f27d464143e8b3fef6c241b
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-08-12 16:00:28 -0400

    feat(lcfetch.h): improve copyright message

--- include/lcfetch.h
+++ include/lcfetch.h
@@ -9,8 +9,15 @@
 
 /* copyright notice */
 #define COPYRIGHT                                                                                                      \
-    "Copyright (c) 2021 NTBBloodbath\nlcfetch is licensed under GPLv2 "                                                \
-    "license."
+    "Copyright (c) 2021 NTBBloodbath. lcfetch is distributed under GPLv2 license.\n\n"                                 \
+    "This program is free software; you can redistribute it and/or "                                                    \
+    "modify it under the terms of the GNU General Public License "                                                      \
+    "as published by the Free Software Foundation; either version 2 "                                                   \
+    "of the License, or (at your option) any later version.\n\n"                                                       \
+    "This program is distributed in the hope that it will be useful, "                                                  \
+    "but WITHOUT ANY WARRANTY; without even the implied warranty of "                                                   \
+    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "                                                    \
+    "GNU General Public License for more details.\n"
 
 /* lcfetch.c */
 #define BUF_SIZE 150
