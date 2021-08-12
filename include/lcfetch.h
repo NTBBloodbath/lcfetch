fcd5293c1f95bb9a07b148da57190b95db2b7879
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-08-12 13:10:44 -0400

    release: initial beta release

--- include/lcfetch.h
+++ include/lcfetch.h
@@ -5,7 +5,7 @@
 #define LCFETCH_H
 
 /* lcfetch version */
-#define VERSION "0.1.0"
+#define VERSION "0.1.0-beta1"
 
 /* copyright notice */
 #define COPYRIGHT                                                                                                      \
