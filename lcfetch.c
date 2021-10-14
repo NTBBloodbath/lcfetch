39e26329e680688821855246505780934ef3fc55
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-10-14 01:07:07 -0400

    fix: logos with special characters were having a weird length

--- lcfetch.c
+++ lcfetch.c
@@ -642,7 +642,7 @@ void print_info() {
     int enabled_fields = get_table_size("enabled_fields");
     if (display_logo) {
         // Get the logo length, substracting the ANSI escapes length
-        int logo_length = is_custom_logo ? custom_ascii_logo.rows : (strlen(logo[0]) - strlen("\e[1;00m"));
+        int logo_length = is_custom_logo ? custom_ascii_logo.rows : (utf8len(logo[0]) - strlen("\e[1;00m"));
         // Get the gap that should be between the logo and the information
         int gap_size = get_option_number("gap");
         char *gap_logo_info = repeat_string(" ", gap_size);
